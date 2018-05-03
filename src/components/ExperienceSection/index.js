4c87b969b4d7b1dc8bb2f8e49204a0b4d51be1d5
Kueterman, Allen <Allen.Kueterman@53.com>
2018-05-03 12:55:30 -0400

    updating logos

--- src/components/ExperienceSection/index.js
+++ src/components/ExperienceSection/index.js
@@ -2,9 +2,9 @@ import React from 'react'
 import Section from '../Section'
 import ExperienceUnit from '../ExperienceUnit'
 
-import zomatoLogo from '../../assets/images/experience/zomato-logo.svg'
-import fitsoLogo from '../../assets/images/experience/fitso-logo.jpg'
-import kayakoLogo from '../../assets/images/experience/kayako-logo-2.jpg'
+import fifthThirdLogo from '../../assets/images/experience/53-logo.png'
+import base11Logo from '../../assets/images/experience/base11-studios.svg'
+import teradataLogo from '../../assets/images/experience/Teradata-Logo.png'
 
 class ExperienceSection extends React.Component {
   render() {
@@ -12,26 +12,26 @@ class ExperienceSection extends React.Component {
       <Section title="Experience">
         <div className="row">
           <ExperienceUnit
-            logo={zomatoLogo}
-            colour='#CB202D'
+            logo={fifthThirdLogo}
+            colour='#FFFFFF'
             title='Fifth Third Bank'
-            link='https://www.zomato.com/'
+            link='https://www.53.com/'
             timeperiod='2014 - current'
             subtitle='Senior Application Developer, developing features for the Fifth Third Bank Mobile Banking application.  Previously an IT Leadership Program participant.'
           />
           <ExperienceUnit
-            logo={fitsoLogo}
+            logo={base11Logo}
             colour='#FFFFFF'
             title='Base11 Studios'
-            link='https://www.getfitso.com/'
+            link='https://www.base11studios.com/'
             timeperiod='2017 - current'
             subtitle='Helped build iOS applications CaliCalo and fuzZzy as well as the Base11 Studios site and blog using Jekyll and GitHub Pages.'
           />
           <ExperienceUnit
-            logo={kayakoLogo}
-            colour='#403949'
+            logo={teradataLogo}
+            colour='#FFFFFF'
             title='Teradata'
-            link='https://www.kayako.com/'
+            link='https://www.teradata.com/'
             timeperiod='2013'
             subtitle='Updated intranet SSO solution using JavaScript and JSP, re-wrote and migrated reporting PL/SQL reporting app to production database.'
           />
